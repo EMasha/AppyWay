782ed0144711820e2a360f84131b5272b0644c0b
EMasha <enxhimasha91@gmail.com>
2026-09-02 06:30:50 -0700

    fixed settings

--- workforce/workforce/settings.py
+++ workforce/workforce/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-1t2k8sl70y)!da-%ehnr_qyu!_zf10-%_hq(s77xiw-t^#@#+e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
